529cf9b7d3ea0ff9b3d8892c23682d77dbb7b1f2
rastaasta <218407492+rastaasta@users.noreply.github.com>
2026-04-24 16:34:40 -0400

    Added some ids and classes to the buttons
    Add the ids and class to use them for js and css. Made the operate
    funtion work.

--- javascript.js
+++ javascript.js
@@ -3,10 +3,16 @@ const substract = ((a, b) => a - b);
 const multiply = ((a, b) => a * b);
 const divide = ((a, b) => a / b);
 
-let num1 = number();
-let operator = number();
-let num2 = number();
+let num1 = null;
+let operator = null;
+let num2 = null;
 
-function operate(operator, num1, num2) {
-    
-};
+const operate = ((operator, a, b) => {
+    switch (operator) {
+        case '+': return add(a, b);
+        case '-': return substract(a, b);
+        case '*': return multiply(a, b);
+        case '/': return divide(a, b);
+        default: return null;
+    }
+});
