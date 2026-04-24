fd979ce3327fc97c895938053c4b838947e92268
rastaasta <218407492+rastaasta@users.noreply.github.com>
2026-04-24 14:51:40 -0400

    Created some functions, varibles and buttons
    Created operation functions to test in console.log. Created 3 varibales
    for the full operation. Added another function called operate. Created
    new buttons for the calculator.

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const add = ((a, b) => a + b);
+const substract = ((a, b) => a - b);
+const multiply = ((a, b) => a * b);
+const divide = ((a, b) => a / b);
+
+let num1 = number();
+let operator = number();
+let num2 = number();
+
+function operate(operator, num1, num2) {
+    
+};
